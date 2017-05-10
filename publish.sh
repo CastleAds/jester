94f35842276393548cfd5aeca361efed3a9bcd78
nkconnor <nicholas.k.connor@gmail.com>
2017-05-10 14:25:02 -0400

    Push latest version, add publish script

---
+++ publish.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git push origin master
+/usr/bin/python3.4 setup.py sdist upload
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jester',
-      version='0.2',
+      version='0.21',
       description='Utilities for web applications in Python3.4',
       url='http://github.com/CastleAds/jester',
       author='Nicholas Connor',
